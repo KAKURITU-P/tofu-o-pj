8b809a6cd114374b6aef8f64d08bf397b7c447c4
確率P <158581461+KAKURITU-P@users.noreply.github.com>
2024-09-18 10:42:22 +0900

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,55 +1,72 @@
-let 無線ー名前 = ""
-let cash = 0
-let LOG = ""
 input.onPinPressed(TouchPin.P0, function () {
-    radio.sendValue(無線ー名前, 0)
-    for (let index = 0; index < 4; index++) {
-        basic.showLeds(`
-            # . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            `)
-        basic.pause(1000)
-        basic.clearScreen()
-        basic.pause(1000)
-    }
-    basic.showString(無線ー名前)
+    radio.sendValue(端末ID, 0)
+    basic.showString(端末ID)
 })
 input.onButtonPressed(Button.A, function () {
-    radio.sendValue(無線ー名前, 1)
-})
-input.onPinPressed(TouchPin.P2, function () {
-    if (true) {
-        cash = 0
+    if ("z" == 端末ID) {
+        受け取り_１番目 = 0
     } else {
-    	
+        radio.sendValue(端末ID, 1)
     }
 })
-input.onPinPressed(TouchPin.P1, function () {
-    basic.showString(LOG.charAt(0))
+input.onPinPressed(TouchPin.P2, function () {
+    if (p2 <= 5) {
+        p2 += 1
+        basic.showString("abcdefz".charAt(p2))
+        端末ID = "abcdefz".charAt(p2)
+    } else {
+        p2 = 0
+        basic.showString("abcdefz".charAt(p2))
+        端末ID = "abcdefz".charAt(p2)
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    if ("z" == 端末ID) {
+        basic.showString(cash)
+    }
 })
 radio.onReceivedValue(function (name, value) {
     if (value == 0) {
-        for (let index = 0; index < 4; index++) {
-            basic.showLeds(`
-                # . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                . . . . .
-                `)
-            basic.pause(1000)
-            basic.clearScreen()
-            basic.pause(1000)
+        basic.showString(name)
+    } else if (value == 1 && 端末ID == "z") {
+        if (0 == 受け取り_１番目) {
+            basic.showString(name)
+            受け取り_１番目 = 1
+            cash = name
         }
-        basic.showString(name)
-    } else if (value == 1) {
-        basic.showString(name)
-        cash = "" + name + LOG
-        LOG = cash
     } else {
     	
+    }
+})
+let cash = ""
+let p2 = 0
+let 受け取り_１番目 = 0
+let 端末ID = ""
+端末ID = "a"
+basic.forever(function () {
+    if ("z" == 端末ID && 受け取り_１番目 == 1) {
+        music.play(music.createSoundExpression(
+        WaveShape.Sine,
+        2400,
+        2400,
+        255,
+        255,
+        200,
+        SoundExpressionEffect.None,
+        InterpolationCurve.Linear
+        ), music.PlaybackMode.InBackground)
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+        basic.pause(100)
+        basic.clearScreen()
+        music.play(music.createSoundExpression(WaveShape.Sine, 2400, 2400, 255, 255, 500, SoundExpressionEffect.None, InterpolationCurve.Linear), music.PlaybackMode.InBackground)
+        basic.showString(cash)
+        basic.pause(100)
+        basic.clearScreen()
     }
 })
